99054a4ad3c9212327eb217cb2a8d8b96f08e3c7
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2023-08-26 14:20:30 +0200

    Docstrings added

--- functions.py
+++ functions.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 
 
 def placeholder_should_be(element_xpath, placeholder_text):
+    """A function that double checks presence and content of an element's
+    placeholder"""
     lib = BuiltIn().get_library_instance("SeleniumLibrary")
     element = lib.driver.find_element(By.XPATH, element_xpath)
     if not element:
@@ -11,6 +13,8 @@ def placeholder_should_be(element_xpath, placeholder_text):
 
 
 def background_image_should_be(background_image_src):
+    """A function that double checks presence and name of a webpage's
+    background"""
     lib = BuiltIn().get_library_instance("SeleniumLibrary")
     element = lib.driver.find_element(By.XPATH, "//body")
     background_image = element.value_of_css_property("background")
